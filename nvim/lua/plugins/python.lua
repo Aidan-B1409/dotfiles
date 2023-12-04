2327b099205e4b7cc653d4829b7c42608c9cea14
Aidan-B1409 <aidanb1049@gmail.com>
2023-12-04 14:09:37 -0800

    Change lazyvim version

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -5,13 +5,13 @@ return {
       table.insert(opts.ensure_installed, "black")
     end,
   },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.black)
-    end,
-  },
+  -- {
+  --   "jose-elias-alvarez/null-ls.nvim",
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     table.insert(opts.sources, nls.builtins.formatting.black)
+  --   end,
+  -- },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
